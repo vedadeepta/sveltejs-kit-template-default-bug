2c6ef08ab1688ea3d42ade5aff969c753a73c8e0
veda-angelone <veda.pal@angelbroking.com>
2023-08-03 17:13:41 +0530

    bug

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,9 +1,8 @@
 export async function load({ fetch }) {
 	// const res = await fetch('/mock');
-
-	// const res = await fetch(new Request('http://localhost:5173/' +
-	const res = await fetch('http://localhost:5173/' + 'mock');
+	// const res = await fetch('http://localhost:5173/' + 'mock');
 	//
+	const res = await fetch(new Request('http://localhost:5173/' + 'mock'));
 	const data = await res.json();
 	return data;
 }
